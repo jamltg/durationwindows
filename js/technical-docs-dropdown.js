0ea7a886863d5ebd73977275e5127b9b55a270f1
jamltg <jamletigio@gmail.com>
2025-04-03 01:30:44 +0800

    added technical-documents changes and added new folders to source code

---
+++ js/technical-docs-dropdown.js
@@ -0,0 +1,5 @@
+function technicalDocsToggleDropdown() {
+    const dropdown = document.getElementById('technical-docs-nav-dropdown-content');
+    // Toggle the display property
+    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+}
